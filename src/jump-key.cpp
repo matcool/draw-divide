b5a768e8d716c19513a2cbff1dd98b35c522775b
matcool <26722564+matcool@users.noreply.github.com>
2024-01-11 16:02:38 -0300

    make IJL act as arrow keys

--- src/jump-key.cpp
+++ src/jump-key.cpp
@@ -5,15 +5,21 @@
 using namespace cocos2d;
 
 void CCKeyboardDispatcher_dispatchKeyboardMSG(void* self, cocos2d::enumKeyCodes key, bool a, bool b) {
-	if (key == cocos2d::KEY_C) {
-		key = cocos2d::KEY_W;
+	switch (key) {
+		// turn C into W
+		case KEY_C: key = KEY_W; break;
+		// turn JIL into <^>
+		case KEY_J: key = KEY_Left; break;
+		case KEY_I: key = KEY_Up; break;
+		case KEY_L: key = KEY_Right; break;
+		default: break;
 	}
 	matdash::orig<&CCKeyboardDispatcher_dispatchKeyboardMSG>(self, key, a, b);
 }
 
 void mod_main(HMODULE) {
-    // matdash::create_console();
-    auto cocos_handle = LoadLibraryA("libcocos2d.dll");
+	// matdash::create_console();
+	auto cocos_handle = LoadLibraryA("libcocos2d.dll");
 
 	matdash::add_hook<&CCKeyboardDispatcher_dispatchKeyboardMSG>(GetProcAddress(cocos_handle, "?dispatchKeyboardMSG@CCKeyboardDispatcher@cocos2d@@QAE_NW4enumKeyCodes@2@_N1@Z"));
 }
